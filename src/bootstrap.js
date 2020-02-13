b2b9aff025207f2c09c42444ebd8b70db243183e
Trent Hendrickson <trentdev2020@gmail.com>
2020-02-12 17:20:05 -0700

    First Init Push!

---
+++ src/bootstrap.js
@@ -0,0 +1,5 @@
+import classChallenge, {addition, myString} from './classChallenge' 
+
+console.log(classChallenge())
+console.log(myString)
+console.log(addition(5, 5))
---
+++ src/classChallenge.js
@@ -0,0 +1,9 @@
+export default function () {
+    return '<div>classChallenge</div>'
+}
+
+export const myString = "welcome";
+
+export function addition(numOne, numTwo) {
+    return numOne + numTwo;
+}
